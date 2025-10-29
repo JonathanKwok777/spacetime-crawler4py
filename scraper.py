b78fcbd16342c90b1a37295a413d659724ae1168
carrieliu6704 <122652023+carrieliu6704@users.noreply.github.com>
2025-10-28 19:36:36 -0700

    added global STOPWORDS list for extract_tokens function

--- scraper.py
+++ scraper.py
@@ -2,6 +2,20 @@ import re
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
+STOPWORDS = set("""
+a about above after again against all am an and any are aren't as at be because been before
+being below between both but by can can't could couldn't did didn't do does doesn't doing don't
+down during each few for from further had hadn't has hasn't have haven't having he he'd he'll
+he's her here here's hers herself him himself his how how's i i'd i'll i'm i've if in into is
+isn't it it's its itself let's me more most mustn't my myself no nor not of off on once only
+or other ought our ours  ourselves out over own same shan't she she'd she'll she's should shouldn't
+so some such than that that's the their theirs them themselves then there there's these they they'd
+they'll they're they've this those through to too under until up very was wasn't we we'd we'll we're
+we've were weren't what what's when when's where where's which while who who's whom why why's with
+won't would wouldn't you you'd you'll you're you've your yours yourself yourselves
+""".split())
+
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -77,4 +91,5 @@ def extract_tokens(text):
     tokens = re.findall(r"[A-Za-z0-9]+", text)
     clean_tokens = [t.lower() for t in tokens if t.lower() not in STOPWORDS]
     return clean_tokens
+
 
